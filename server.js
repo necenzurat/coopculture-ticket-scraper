a816c2320818fa4fe8e9fa4657822f10e39d0389
necenzurat <necenzurat@gmail.com>
2023-05-29 17:47:47 +0300

    yey

--- server.js
+++ server.js
@@ -2,61 +2,74 @@ import open from "open";
 import fetch from 'node-fetch';
 import { parse } from 'node-html-parser';
 
-function wait(time = 500) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve("done!");
-    }, time);
-  });
-}
+
+
+
+const data = '30/05/2023';
+const ora = '11:00';
+const toBeParsed = 'https://ecm.coopculture.it/index.php?option=com_snapp&view=event&id=705EC694-B38F-2FD3-0D89-01869979E42C&catalogid=619C4D83-5100-83D4-1732-01869980308C&lang=en';
+
+
+
+
 
 
 async function main() {
-  let response;
+  let listEvents;
   let i = 0;
+  let url = new URL(toBeParsed);
+
+  let eventID = url.searchParams.get('id');
+  let catalogID = url.searchParams.get('catalogid');
 
   while (true) {
+  try {
 
-    try {
-      response = await fetch("https://ecm.coopculture.it/index.php?option=com_snapp&task=event.getperformancelist&format=raw&id=3C38AB77-8D5A-5394-05B2-0172EB8E7D46&type=1&date_req=30/03/2023&dispoonly=1&lang=en&_=1678676357632");
+      listEvents = await fetch("https://ecm.coopculture.it/index.php?option=com_snapp&task=event.getperformancelist&format=raw&id="+eventID+"&type=1&date_req="+data+"&dispoonly=1&lang=en&_=1685079701737");
 
-
-      let body = await response.text();
+      let body = await listEvents.text();
       let html = parse(body);
+      let children = html.querySelectorAll('.perf_row');
+      if (children.length >= 1){
+        console.log(children.length);
+        for (let i1 = 0; i1 < children.length; i1++){
+          let child = children[i1];
+          let hour = child.querySelector('div').rawText.trim();
+          let tickets = child.querySelector('.brfrmnc-remaining').innerText.trim().replace(/([()])/g, '')
+          let TicketsNO = 1;
+          let performanceID = child.querySelector('button').getAttribute('data-performanceid');
 
-      const children = html.querySelectorAll('.perf_row');
+          console.info("La ora "+ hour +" mai erau bilete "+tickets+" disponibile");
 
-      for (let i1 = 0; i1 < children.length; i1++){
-        const child = children[i1];
-         const hour = child.querySelector('div').rawText.trim();
+          let getProductHtml = await fetch("https://ecm.coopculture.it/index.php?option=com_snapp&task=event.getSellableProducts&format=raw&performanceId="+performanceID+"&catalogId="+catalogID+"&eventId="+eventID+"&lang=en&_=1685079869084");
+          let productHtml = await getProductHtml.text();
+          let checkout = parse(productHtml);
 
-         const tickets = child.querySelector('.brfrmnc-remaining').innerText.trim().replace(/([()])/g, '')
+          let productID = checkout.querySelector('input').getAttribute('data-productId');
 
-         const performanceid = child.querySelector('button').getAttribute('data-performanceid');
-
-         console.log(hour, tickets, performanceid);
-
-        const aprox = hour.match('17');
+          let aprox = hour.match(ora);
 
          if(aprox){
            await open("https://ecm.coopculture.it/index.php?option=com_snapp&task=cart.cart"
-           +"&productid=12837B39-8EA6-E674-075D-0172EB8E8A52"
-           +"&quantity="+tickets
-           +"&performanceid="+performanceid
-           +"&catalogId=F3CB77BD-6A43-108B-6723-0174490EB610&"
-           +"&eventId=3C38AB77-8D5A-5394-05B2-0172EB8E7D46&"
+           +"&productid="+productID
+           +"&quantity="+TicketsNO
+           +"&performanceid="+performanceID
+           +"&catalogId="+catalogID
+           +"&eventId="+eventID
            +"&format=raw&lang=en&options=");
            break;
          }
       }
+  }
+
+
 
     } catch (e) {
         console.log(e);
     }
 
     i++;
-    await wait(500);
+  console.log(i);
   }
-
 }
 main();
